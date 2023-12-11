d537d2e7178e04e3a77536e0531ee1d39750370b
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2023-12-11 20:04:37 +0100

    Get the numeric value in the string

--- 2023/1-1/1-1.cbl
+++ 2023/1-1/1-1.cbl
@@ -1,30 +1,27 @@
-       IDENTIFICATION DIVISION.
+       identification division.
        PROGRAM-ID. 1-1.
-       DATE-WRITTEN. 10.12.2023
-       AUTHOR. cyco.
 
        DATA DIVISION.           
        WORKING-STORAGE SECTION.
-       77 strvaleur PIC X(50).
-       77 FirstNumericVal PIC 9.
-       77 Last-Numeric-Values PIC 9.
+       77 strvaleur PIC X(30).
+      *77 FirstNumericVal PIC 9.
+      *77 Last-Numeric-Values PIC 9.
        77 Counter PIC 9(3) VALUE 1.
 
        SCREEN SECTION.
        1 s-plg-strvaleur.
           2 LINE 5 COL 8 VALUE 'Quel est votre nom : '.
-          2 PIC X(25) TO strvaleur REQUIRED.
+          2 PIC X(50) TO strvaleur REQUIRED.
 
        PROCEDURE DIVISION.
-          ACCEPT s-plg-strvaleur.
+           ACCEPT s-plg-strvaleur.
 
-          PERFORM VARYING Counter FROM 1 BY 1
-           UNTIL Counter > LENGTH OF strvaleur OR 
-               strvaleur(Counter:1) IS NUMERIC
-           MOVE strvaleur(Counter) TO FirstNumericVal
-                   EXIT PERFORM
-          END-PERFORM.
+           perform find-first-num varying Counter from 1 by 1
+           until Counter > length OF strvaleur.
 
-          DISPLAY FirstNumericVal.
+           STOP RUN.
 
-          STOP RUN.
+           find-first-num.
+               IF strvaleur(Counter:1) IS numeric
+               DISPLAY strvaleur(COunter:1) COL Counter.
+      *         END-IF.
