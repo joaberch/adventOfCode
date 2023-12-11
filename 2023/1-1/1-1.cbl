b4f494f2f5895696219246ef5ebb7f1e06c1c485
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2023-12-11 20:27:52 +0100

    Get the first and the last numeric value of a string

--- 2023/1-1/1-1.cbl
+++ 2023/1-1/1-1.cbl
@@ -4,9 +4,10 @@
        DATA DIVISION.           
        WORKING-STORAGE SECTION.
        77 strvaleur PIC X(30).
-      *77 FirstNumericVal PIC 9.
-      *77 Last-Numeric-Values PIC 9.
+       77 FirstNumericVal PIC 9.
+       77 Last-Numeric-Values PIC 9.
        77 Counter PIC 9(3) VALUE 1.
+       77 FoundNumericValue PIC X VALUE 'N'.
 
        SCREEN SECTION.
        1 s-plg-strvaleur.
@@ -15,13 +16,25 @@
 
        PROCEDURE DIVISION.
            ACCEPT s-plg-strvaleur.
+           
+           perform findval varying Counter from 1 by 1
+           until Counter > length of strvaleur.
 
-           perform find-first-num varying Counter from 1 by 1
-           until Counter > length OF strvaleur.
+           DISPLAY FirstNumericVal COL 1.
+           DISPLAY Last-Numeric-Values COL 5.
 
            STOP RUN.
 
-           find-first-num.
+           findval.
+               IF strvaleur(Counter:1) IS NUMERIC
+               AND FoundNumericValue = 'N'
+               SET FirstNumericVal TO strvaleur(Counter:1)
+               SET Last-Numeric-Values TO strvaleur(Counter:1)
+      *         SET strvaleur(Counter:1) TO FirstNumericVal.
+      *         SET strvaleur(Counter:1) TO Last-Numeric-Values.
+               MOVE 'Y' TO FoundNumericValue.
+
                IF strvaleur(Counter:1) IS numeric
-               DISPLAY strvaleur(COunter:1) COL Counter.
-      *         END-IF.
+               SET Last-Numeric-Values TO strvaleur(Counter:1)
+               EXIT.
+               
